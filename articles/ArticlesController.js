e272d4092b2330727accbe4cbddc70abbae72943
Eduardo <edp2013.ep@gmail.com>
2019-11-18 21:59:27 -0200

    Edição de artigo pronta!

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -50,5 +50,48 @@ router.post("/articles/delete", (req, res) => {
     res.redirect("/admin/articles");
   }
 });
+//Editar
+router.get("/admin/articles/edit/:id", (req, res) =>{
+  var id = req.params.id;
+  if(isNaN(id)){
+    res.redirect("/admin/articles")
+  }
+  Articles.findByPk(id).then(article =>{
+    if(article != undefined){
+      Category.findAll().then(categories => {
+        res.render("admin/articles/edit", {
+          article: article,
+          categories: categories
 
+        });
+      });
+      
+    }else{
+      res.redirect("/admin/articles")
+    }
+    }).catch(erro => {
+      res.redirect("/admin/articles");
+  });
+})
+//Update
+router.post("/articles/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category
+
+  Articles.update({
+      title: title, 
+      body: body,
+      categoryId: category
+     },
+    {
+      where: {
+        id: id
+      }
+    }
+  ).then(() => {
+    res.redirect("/admin/articles");
+  });
+});
 module.exports = router;
