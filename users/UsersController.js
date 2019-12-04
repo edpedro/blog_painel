7644e82fc7afd06e71577efea8bca77b6c20f310
Eduardo <edp2013.ep@gmail.com>
2019-12-03 21:24:30 -0300

    Logout pronto!

--- users/UsersController.js
+++ users/UsersController.js
@@ -61,4 +61,8 @@ router.post("/authenticate", (req, res) =>{
       }
     })
 })
+router.get("/logout", (req, res) =>{
+  req.session.user = undefined;
+  res.redirect("/")
+})
 module.exports = router
